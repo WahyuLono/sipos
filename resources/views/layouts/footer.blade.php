213bceb9999d8750c8c37568e8dd400c01349561
wahyulono <wahyuyuwono@gmail.com>
2024-01-02 11:45:38 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>SiPos © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://www.linkedin.com/in/wahyu-yuwono/">Wahyu Yuwono</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
